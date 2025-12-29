db42ef669362c10f966c0246baea599b54dcd999
asmaa-aljazzar <asma.jazar.319@gmail.com>
2025-12-29 16:08:12 +0300

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({movie_app
-  base: '/movie_app/', 
-  plugins: [react(), tailwindcss()],
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	base: '/movie_app/' 
 })
